6b84e9bec6c544ea3641c2d45cfbc081fc85d798
Mathieu Le Roux <mathieulr@gmail.com>
2016-08-22 18:49:16 -0300

    crud place

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,44 @@
+class PlacesController < ApplicationController
+
+  def index
+    @user = current_user
+    @places = @user.places.all
+  end
+
+  def show
+    @place = Place.find(params[:id])
+  end
+
+  def new
+    @place = Place.new
+  end
+
+  def create
+    @place = Place.new(place_params)
+    @place.user = current_user
+    @place.save
+    redirect_to places_path
+  end
+
+  def edit
+    @place = Place.find(params[:id])
+  end
+
+  def update
+    @place = Place.find(params[:id])
+    @place.update(place_params)
+    redirect_to places_path
+  end
+
+  def destroy
+    @place = Place.find(params[:id])
+    @place.destroy
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:address, :name, :user)
+  end
+
+end
