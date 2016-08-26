b9f59d8fe12ebc1155fbd9deb10440da0f44a3ce
Mathieu Le Roux <mathieulr@gmail.com>
2016-08-26 17:29:58 -0300

    avatar corrected without password & default

---
+++ app/controllers/invites_controller.rb
@@ -0,0 +1,14 @@
+class InvitesController < ApplicationController
+  def show
+    @user = current_user
+    @place = Place.find(params[:id])
+    authorize @place
+  end
+
+  def update
+    skip_authorization
+    @place = Place.find(params[:id])
+    PlaceMailer.ask_invitation(@place, current_user).deliver_now
+    redirect_to root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers:{ registrations: 'registrations' }
   root to: 'places#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :places do
