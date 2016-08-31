27e35684391d11f7e65440b076eb4addf454c19f
Mathieu Le Roux <mathieulr@gmail.com>
2016-08-30 23:11:21 -0300

    debugged by adding scope in overlap on booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,5 +3,5 @@ class Booking < ApplicationRecord
   belongs_to :user
   validates :start_time, presence: true
   validates :end_time, presence: true
-  validates :start_time, :end_time, overlap: true
+  validates :start_time, :end_time, :overlap => {:scope => "user_id"}
 end
