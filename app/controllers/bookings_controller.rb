8d9b5b585c9e52f2d78730f678bb8ed83f9cf202
roger scott <rogerpscott@gmail.com>
2016-08-31 10:25:16 -0300

    not bookable in the past and in not bookable days

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -16,15 +16,17 @@ class BookingsController < ApplicationController
     @booking.place = @place
     @booking.user = @user
     authorize @booking
-    if @booking.start_time < @booking.end_time
-      if @booking.save
-        BookingMailer.creation_confirmation(@booking).deliver_now
-        redirect_to bookings_path
-      else
-        redirect_to place_path(@place), alert: "#{@place.name} is not available at that time. Please check the calendar."
-      end
-    else
+    if @booking.start_time < DateTime.now
+      redirect_to place_path(@place), alert: "You cannot book a date in the past"
+    elsif @booking.start_time > @booking.end_time
       redirect_to place_path(@place), alert: "Please choose a start time that is before the end time"
+    elsif ( get_days_of_booking(@booking) & @place.non_available_day ).any?
+      redirect_to place_path(@place), alert: "#{@place.name} is not available during these days. Please check the calendar."
+    elsif @booking.save
+      BookingMailer.creation_confirmation(@booking).deliver_now
+      redirect_to bookings_path
+    else
+      redirect_to place_path(@place), alert: "#{@place.name} is not available at that time. Please check the calendar."
     end
   end
 
@@ -49,5 +51,19 @@ class BookingsController < ApplicationController
   def booking_params
     # on doit autoriser le champs de l'élément caché place_id
     params.require(:booking).permit(:start_time, :end_time, :place_id)
+  end
+
+  def get_days_of_booking(booking)
+    js = booking.start_time.strftime("%j").to_i
+    je = booking.end_time.strftime("%j").to_i
+    s = booking.start_time.strftime("%w").to_i
+    e = booking.end_time.strftime("%w").to_i
+    if je - js > 5
+      return (0..6).to_a
+    elsif s <= e
+      return (s..e).to_a
+    else
+      return (0..6).to_a.reject { |i| ((e + 1)...s).include? i}
+    end
   end
 end
