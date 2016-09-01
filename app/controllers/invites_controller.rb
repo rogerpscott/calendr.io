bc61677c4ddba398f7d5b11a9831f0ee56ddfdf9
jonathan chevalier <chevalier.jonathan@live.Fr>
2016-09-01 18:37:51 -0300

    added background form for email invitation and changed color alert for invitation email

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -9,6 +9,6 @@ class InvitesController < ApplicationController
     skip_authorization
     @place = Place.friendly.find(params[:id])
     PlaceMailer.ask_invitation(@place, current_user).deliver_now
-    redirect_to root_path, alert: "Your request for access was sent to the owner, you'll receive an email when it's accepted."
+    redirect_to root_path, notice: "Your request for access was sent to the owner, you'll receive an email when it's accepted."
   end
 end
